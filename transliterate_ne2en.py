c413808588cbee0dd586af1292f9dff8e24e3e2b
Prabhas Pokharel <prabhas.pokharel@gmail.com>
2011-06-21 17:42:35 +0545

    Made the schwa rule better (for nouns). Doesn't work so well for vowels unfortunately :(

--- transliterate_ne2en.py
+++ transliterate_ne2en.py
@@ -4,9 +4,11 @@ import codecs,re,optparse,sys
 
 # This is a quick python script to transliterate Nepali (Devnagari) to English.
 # It uses a lossy transliteration that I defined to do the work, and converts to all Ascii characters (ie, no diacritics), and doesn't use capital / small letters.
-# The issue of the schwa (ie, the sometimes implicit a at the end of words (ie, बन्द becomes band or banda? नाम becomes naam or naama?)) is hard to deal with, so I have provided a flag to deal with the issues. 
-#     -s or --inclue-schwa outputs banda and naama; by default the schwa is ignored
-# Also, not sure what to do with अं (for now (m)) and अँ (for now (n))
+# The issue of the schwa (ie, the sometimes implicit a at the end of words (ie, बन्द becomes band or banda? नाम becomes naam or naama?)) is dealt of in the following way:
+#     If there is a punctuation or a halanta preceding the consonant, schwa is added (ex: बान्द => banda, म => ma)
+#     However, if what precedes is a vowel, then no schwa is added (ex: नाााम => naam, प्रसाद => prasaad)
+#     This is not a perfect rule: works well for nouns (see above) but not for verbs (पर्दैन => pardaina)
+#     But its the best for right now
  
 def unicodify(strdict):
 	return dict([(unicode(x[0],'utf-8'),unicode(x[1],'utf-8')) for x in strdict.items()])
@@ -24,23 +26,31 @@ def multiple_replace(dictionary, text):
 	return regex.sub(lambda mo: dictionary[mo.string[mo.start():mo.end()]], text)
 
 
-def transliterate(string, includeschwa=False):
+def transliterate(string):
 	if isinstance(string, basestring) and not isinstance(string, unicode): string = string.encode("utf-8") # make sure we have unicode
 	ret = ''
 	string = multiple_replace(preprocess, string)
 	for i,v in enumerate(string):
-		# akarsukars and vowels are simple 
+		# akars, ukars and vowels are simple: you just transliterate based on the table.
 		if akar_ukar.has_key(v):
 			ret = ret + akar_ukar[v]
 	 	elif vowels.has_key(v):	
 			ret = ret + vowels[v]
-		# for consonants, depends on what follows. If its not a word end, and not an ukar_akar, then a is added 
+		# Consonants are tricky (क is ka or just k depending on what follows).
 		elif consonants.has_key(v):
-			if (i+1 < len(string) and not akar_ukar.has_key(string[i+1])):
-				if not includeschwa and string[i+1] in punctuations: ret = ret + consonants.get(v,v)
-				else: ret = ret + consonants.get(v,v) + 'a'
-			else:
+			# If the consonant is followed by an akar or an ukar things are simple (see else)
+			if i < len(string) and akar_ukar.has_key(string[i+1]):
 				ret = ret + consonants.get(v,v)
+			# If not, we have to check whether the consonant is at a word's end, and go into schwa rules
+			else:
+				if i == len(string) or string[i+1] in punctuations: #if its a schwa
+					#Schwa rule: 
+					if i > 0 and (string[i-1] == u'्' or string[i-1] in punctuations): # if preceding char is halanta or a punctuation
+						ret = ret + consonants.get(v,v) + 'a'
+					else:
+						ret = ret + consonants.get(v,v)
+				else: #no schwa and no akar. means we have an अ following the consonant
+					ret = ret + consonants.get(v,v) + 'a'
 		else: ret = ret + v
 	return ret
 
@@ -49,8 +59,6 @@ if __name__=="__main__":
 	parser = optparse.OptionParser()
 	parser.add_option("-v", action="store_true", dest="verbose",
 		  help="Verbose mode: give output about status.")
-	parser.add_option("-s", action="store_true", dest="schwa",
-		  help="Include the schwa (end words with an a if no vowel ends word). Example: naama, banda, etc.",metavar="SCHWA")
 	parser.add_option("-o", "--output", dest="outputfile",
 		  help="Output filename. Prints to stdout if unspecified", metavar="OUTFILE")
 	(options, args) = parser.parse_args()
@@ -63,9 +71,9 @@ if __name__=="__main__":
 	outf = options.outputfile
 	if outf:
 		f = codecs.open(outf, "w", 'utf-8')
-		f.write(transliterate(input,includeschwa=options.schwa))
+		f.write(transliterate(input))
 		f.close()
 		if options.verbose: print "Output written to ",outf
 	else:
 		if options.verbose: print "Output commencing..."
-		print transliterate(input,includeschwa=options.schwa)
+		print transliterate(input)
